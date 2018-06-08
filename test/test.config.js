dcfbb9a6232171ce234c18e22e2e757e182e69a0
Ganesh Annan <gannan@digitalbazaar.com>
2018-06-08 14:39:53 -0400

    Various fixes.

--- test/test.config.js
+++ test/test.config.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
  */
 
-const config = require('bedrock').config;
+const {config} = require('bedrock');
 const path = require('path');
 
 config.karma.suites['bedrock-web-store'] = path.join('web', '**', '*.js');
